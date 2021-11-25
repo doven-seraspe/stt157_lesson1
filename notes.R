01ec83e67e71c97ae54a7e87710ee7095bdbd8cc
unknown <dovseraspe@gmail.com>
2021-11-25 23:26:52 +0800

    added chapters 1 to 3 with reference

---
+++ notes.R
@@ -0,0 +1,14 @@
+# {-} this means to tangtang the number
+
+stringi::stri_rand_lipsum(1) # this will run a random paragraph
+
+#then
+`r stringi::stri_rand_lipsum(1)`
+
+#in terminal
+# touch myref.bib to create a reference file in rstudio
+
+# bibliography: ["myRef.bib"] to call out your references
+
+devtools::install_github("crsh/citr")
+
